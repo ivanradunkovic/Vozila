9012399e028870705d9c3abeaa1e1a4ef9319e0a
Ivan Radunkovic <ivan1501@gmail.com>
2019-11-21 11:46:55 +0100

    Initiall commit

---
+++ Vozila/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vozila.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult VehicleMake()
+        {
+            ViewBag.Message = "Your Vehicle Make";
+
+            return View();
+        }
+
+        public ActionResult VehicleModel()
+        {
+            ViewBag.Message = "Your Vehicle Model";
+
+            return View();
+        }
+
+        public ActionResult AboutAuthor()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
