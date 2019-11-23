8d1d1be8fa38874408d73a4aca060c4313c8311e
Ivan Radunkovic <ivan1501@gmail.com>
2019-11-23 13:24:10 +0100

    Changes to database. Id change in model

--- Vozila/DAL/VehicleInitializer.cs
+++ Vozila/DAL/VehicleInitializer.cs
@@ -27,18 +27,18 @@ namespace Vozila.DAL
             new Model{Id=1,MakeId=1,Name="128",Abrv="BMW"},
             new Model{Id=2,MakeId=1,Name="325",Abrv="BMW"},
             new Model{Id=3,MakeId=1,Name="X5",Abrv="BMW"},
-            new Model{Id=1,MakeId=2,Name="Mondeo",Abrv="Ford"},
-            new Model{Id=2,MakeId=2,Name="Fiesta",Abrv="Ford"},
-            new Model{Id=3,MakeId=2,Name="Escort",Abrv="Ford"},
-            new Model{Id=1,MakeId=3,Name="Gold",Abrv="VW"},
-            new Model{Id=2,MakeId=3,Name="Passat",Abrv="VW"},
-            new Model{Id=3,MakeId=3,Name="Arteon",Abrv="VW"},
-            new Model{Id=1,MakeId=4,Name="i30",Abrv="Hyundai"},
-            new Model{Id=2,MakeId=4,Name="Tuscon",Abrv="Hyundai"},
-            new Model{Id=3,MakeId=4,Name="Kona",Abrv="Hyundai"},
-            new Model{Id=1,MakeId=5,Name="Chevroler",Abrv="GM"},
-            new Model{Id=2,MakeId=5,Name="Buick",Abrv="GM"},
-            new Model{Id=3,MakeId=5,Name="Cadillac",Abrv="GM"}
+            new Model{Id=4,MakeId=2,Name="Mondeo",Abrv="Ford"},
+            new Model{Id=5,MakeId=2,Name="Fiesta",Abrv="Ford"},
+            new Model{Id=6,MakeId=2,Name="Escort",Abrv="Ford"},
+            new Model{Id=7,MakeId=3,Name="Gold",Abrv="VW"},
+            new Model{Id=8,MakeId=3,Name="Passat",Abrv="VW"},
+            new Model{Id=9,MakeId=3,Name="Arteon",Abrv="VW"},
+            new Model{Id=10,MakeId=4,Name="i30",Abrv="Hyundai"},
+            new Model{Id=11,MakeId=4,Name="Tuscon",Abrv="Hyundai"},
+            new Model{Id=12,MakeId=4,Name="Kona",Abrv="Hyundai"},
+            new Model{Id=13,MakeId=5,Name="Chevroler",Abrv="GM"},
+            new Model{Id=14,MakeId=5,Name="Buick",Abrv="GM"},
+            new Model{Id=15,MakeId=5,Name="Cadillac",Abrv="GM"}
             };
             models.ForEach(s => context.Models.Add(s));
             context.SaveChanges();
