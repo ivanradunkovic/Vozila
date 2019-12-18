5b625b2efe213563b2fa7b458fa95b54bf4307ba
Ivan <ivan1501@gmail.com>
2019-12-18 11:08:09 +0100

    Remove unused usings

--- Vozila.DAL/Mapping/VehicleModelMap.cs
+++ Vozila.DAL/Mapping/VehicleModelMap.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Vozila.DAL.Entities;
+﻿using Vozila.DAL.Entities;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity.ModelConfiguration;
 
--- Vozila.DAL/VehicleContext.cs
+++ Vozila.DAL/VehicleContext.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.Entity;
-using Vozila.Models;
+﻿using System.Data.Entity;
 using Vozila.DAL.Entities;
 using Vozila.DAL.Mapping;
 
